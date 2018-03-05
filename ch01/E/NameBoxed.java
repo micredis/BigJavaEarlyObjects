01e38db5473224d4fa7099d271dfc3ec735b941d
micredis <micredis@gmail.com>
2018-03-05 11:35:49 +0200

    Add NameBoxed and NameLarge

---
+++ ch01/E/NameBoxed.java
@@ -0,0 +1,11 @@
+/* Write a program that displays your name inside a box on the screen.
+Do your best to approximate lines with characters such as | - + .
+*/
+
+public class NameBoxed {
+	public static void main(String[] args) {
+		System.out.println("+------+");
+		System.out.println("| Dave |");
+		System.out.println("+------+");
+	}
+}
---
+++ ch01/E/NameLarge.java
@@ -0,0 +1,19 @@
+/* Write a program that prints your name in large letters, such as
+
+*   *    **    ****    ****   *   *
+*   *   *  *   *   *   *   *  *   *
+*****  *    *  ****    ****    * *
+*   *  ******  *   *   *   *    *
+*   *  *    *  *    *  *    *   *
+
+*/
+
+public class NameLarge {
+	public static void main(String[] args) {
+		System.out.println("*   *    **    ****    ****   *   *");
+		System.out.println("*   *   *  *   *   *   *   *  *   *");
+		System.out.println("*****  *    *  ****    ****    * *");
+		System.out.println("*   *  ******  *   *   *   *    *");
+		System.out.println("*   *  *    *  *    *  *    *   *");
+	}
+}
