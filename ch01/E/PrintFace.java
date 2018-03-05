92205e1292663ebdf05b35da941041b67b963889
micredis <micredis@gmail.com>
2018-03-05 13:20:52 +0200

    Add PrintFace to E

---
+++ ch01/E/PrintFace.java
@@ -0,0 +1,21 @@
+/* Write a program that prints a face similar to (but different from) the following:
+
+  /////
+ +"""""+
+(| o o |)
+ |  ^  |
+ | '-' |
+ +-----+
+
+ */
+
+ public class PrintFace {
+ 	public static void main(String[] args) {
+ 		System.out.println(" \\\\\\|/// ");  // It may look worng due to the escape sequence; looks OK if printed.
+ 		System.out.println(" +-----+ ");
+ 		System.out.println("<| O O |>");
+ 		System.out.println(" |  !  |");
+ 		System.out.println(" |  U  |");
+ 		System.out.println("  \\___/");
+ 	}
+ }
