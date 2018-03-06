2153662c727118c14b04133aea3cd64e32c35122
micredis <micredis@gmail.com>
2018-03-06 13:30:41 +0200

    Add PietMondrianPainting to E

---
+++ ch01/E/PietMondrianPainting.java
@@ -0,0 +1,32 @@
+/* Write a program that prints an imitation of a Piet Mondrian painting. (Search the
+Internet if you are not familiar with his paintings.) Use character sequences such as
+@@@ or ::: to indicate different colors, and use - and | to form lines.
+*/
+
+public class PietMondrianPainting {
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|---------|@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|         |@@@@@@@@@@@@@@@@@@@@@@@@@@|");
+		System.out.println("|------------------------------------|");
+		System.out.println("|:::::::::|                   |      |");
+		System.out.println("|:::::::::|                   |      |");
+		System.out.println("|:::::::::|                   |------|");
+		System.out.println("|:::::::::|                   |######|");
+		System.out.println("|:::::::::|                   |######|");
+		System.out.println("|:::::::::|                   |######|");
+		System.out.println("|:::::::::|                   |######|");
+		System.out.println("--------------------------------------");
+	}
+}
