a78e598f41f2ba3c48b484d30e8eb025de1a0170
micredis <micredis@gmail.com>
2018-03-09 19:42:53 +0200

    Change frogSays(String message, inl lineMaxLength) in AnimalGreeting to print the message in a number of lines of the specified length

--- ch01/E/AnimalGreeting.java
+++ ch01/E/AnimalGreeting.java
@@ -19,9 +19,10 @@ public class AnimalGreeting {
 	}
 
 	public void go() {
-		System.out.print("Enter a short message: ");
+		System.out.println("Enter a short message: ");
 		String message = readln();
-		frogSays(message);
+		// split message into lines with maximum length of 39 characters
+		frogSays(message, 39);
 	}
 
 	public String readln() {
@@ -36,32 +37,53 @@ public class AnimalGreeting {
 		return line;
 	}
 
-	public void frogSays(String message) {
-		String[] textChunks = message.split("\\s");
+	public void frogSays(String message, int lineMaxLength) {
+		String[] words = message.split(" ");
+		int charCount = 0;
+		int spaceCount = 0;
+		for (int i = 0; i < words.length; ) {
+			String word = words[i];
+			if (word.length() > lineMaxLength) {
+				String fore = word.substring(0, lineMaxLength);
+				String hide = word.substring(lineMaxLength);
+				word = fore;
+				words[i] = hide;
+			}
+			if (charCount + spaceCount + word.length() < lineMaxLength) {
+				System.out.print(word + " ");
+				charCount += word.length();
+				spaceCount++;
+				i++;
+			} else if (word.length() == lineMaxLength) {
+				System.out.println();
+				System.out.println(word);
+				charCount = 0;
+				spaceCount = 0;
+			} else {
+				System.out.println();
+				charCount = 0;
+				spaceCount = 0;
+			}
+		}
+		System.out.println();
+
+		/*String[] textChunks = message.split("\\s");
 		System.out.print("           ");
-		for (int i = 0; i < message.length(); i++) {
+		for (int i = 0; i < message.length() + 2; i++) {
 			System.out.print("_");
 		}
-		
-		/*System.out.println();
-		System.out.print("          (");
-		for (int j = 0; j < 20; j++) {
-			System.out.print(message.substring(j, j + 1) + ")");
-		}*/
-
-		for (int j = 0; j < textChunks.length; j++) {
-			System.out.println("          (" + textChunks[j] + ")");
-		}
-		/*System.out.print("             ");
-		for (int k = 0; k < message.length(); k++) {
-			System.out.print("-");
-		}*/
 		System.out.println();
-		System.out.println("          (___)");		
-		System.out.println("          (_)");		
+		for (int j = 0; j < textChunks.length; j++) {
+			System.out.println("          < " + textChunks[j] + " >");
+		}
+		System.out.print("           ");
+		for (int i = 0; i < message.length() + 2; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
 		System.out.println("         O");		
 		System.out.println("  0__0  o");
 		System.out.println(" (    )");
-		System.out.println("(_m__m_)");
+		System.out.println("(_m__m_)");*/
 	}
 }
