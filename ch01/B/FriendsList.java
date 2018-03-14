5d7026d15e9f500d03ad4c0539b2cf6acafcf02f
micredis <micredis@gmail.com>
2018-03-14 18:15:35 +0200

    Create a static map of friends/dobs and printFriendsList()

--- ch01/B/FriendsList.java
+++ ch01/B/FriendsList.java
@@ -5,11 +5,26 @@ column, print the names of your best friends; in the second, print their birthda
 import java.util.*;
 
 public class FriendsList {
-	//private Map<String, 
+	private static final Map<String, Calendar> friends;
+	static {
+		Map<String, Calendar> map = new TreeMap<>();
+		map.put("Alice", new GregorianCalendar(1980, 0, 15));
+		map.put("Bob", new GregorianCalendar(1977, 1, 27));
+		map.put("Charlie", new GregorianCalendar(1982, 5, 30));
+		friends = Collections.unmodifiableMap(map);
+	}
+
 	public static void main(String[] args) {
-		//new FriendsList().printFriendsList(Alice, Bob, Charlie);
-		Calendar birthday = new GregorianCalendar();
-		System.out.println(birthday.get(Calendar.YEAR) + "-"
-			+ (int) (birthday.get(Calendar.MONTH) + 1) + "-" + birthday.get(Calendar.DATE));
+		new FriendsList().printFriendsList(friends);
+	}
+
+	public void printFriendsList(Map<String, Calendar> friends) {
+		System.out.format("%20s | BIRTHDAY%n", "NAME");
+		System.out.println("-------------------------------");
+		for (Map.Entry<String, Calendar> friend : friends.entrySet()) {
+			String name = friend.getKey();
+			Calendar dob = friend.getValue();
+			System.out.format("%20s | %tD%n", name, dob);
+		}
 	}
 }
