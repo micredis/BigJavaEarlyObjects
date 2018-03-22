b82d81606a2b5a442fc52da32ab1596fcb093171
micredis <micredis@gmail.com>
2018-03-23 00:00:14 +0200

    Start RobotWindowsCounter project. Add classes Room and RobotWindowsCounterTester, enum Layout and a stub for Robot

---
+++ ch01/Projects/RobotWindowsCounter/Room.java
@@ -0,0 +1,22 @@
+public class Room {
+	public static Layout[][] generateRoom(int width, int height) {
+		Layout[][] plan = new Layout[height][width];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if ((i == 0 || i == height - 1) && 
+					(j == 0 || j == width - 1)) {
+					plan[i][j] = Layout.WALL;
+				} else if ((i == 0 || i == height - 1 ||
+					j == 0 || j == width - 1) && (Math.random() > 0.2)) {
+					plan[i][j] = Layout.WALL;
+				} else if (i == 0 || i == height - 1 ||
+					j == 0 || j == width - 1) {
+					plan[i][j] = Layout.WINDOW;
+				} else {
+					plan[i][j] = Layout.FLOOR;
+				}
+			}
+		}
+		return plan;
+	}
+}
