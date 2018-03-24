f0557b0c2932e076dec7f777661bb326df68f63b
micredis <micredis@gmail.com>
2018-03-24 16:23:36 +0200

    Modify Room so the proximity of any window to any corner wouldn't be less than 1 wall unit

--- ch01/Projects/RobotWindowCounter/Room.java
+++ ch01/Projects/RobotWindowCounter/Room.java
@@ -4,7 +4,15 @@ public class Room {
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
 				if ((i == 0 || i == height - 1) && 
-					(j == 0 || j == width - 1)) {
+					(j == 0 || j == width - 1) ||
+					(i == 0 && j == 1 ||
+						i == 0 && j == width - 2 ||
+						i == 1 && j == 0 ||
+						i == 1 && j == width - 1 ||
+						i == height - 2 && j == 0 ||
+						i == height - 2 && j == width - 1 ||
+						i == height - 1 && j == 1 ||
+						i == height - 1 && j == width  - 2)) {
 					plan[i][j] = Layout.WALL;
 				} else if ((i == 0 || i == height - 1 ||
 					j == 0 || j == width - 1) && (Math.random() > 0.2)) {
