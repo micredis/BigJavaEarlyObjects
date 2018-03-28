2287a0ab1db221f9f2f554dc3e92f2d84eaaeb15
micredis <micredis@gmail.com>
2018-03-28 14:26:26 +0300

    Check if the width and the height parameters are entered as arguments at startup. Use these arguments to printCanvas() in PietMondrianTester

--- ch01/Practice/PietMondrian/PietMondrianTester.java
+++ ch01/Practice/PietMondrian/PietMondrianTester.java
@@ -29,111 +29,29 @@ E.g.:
 */
 
 public class PietMondrianTester {
-	public static void main(String[] args) throws InterruptedException {
-		System.out.println(Layout.COLOR1.ordinal());
-		System.out.println(Layout.getRandomColor());
-		Layout[][] canvas = Canvas.generateCanvas(38, 23);
-		printCanvas(canvas);
+	private static int width;
+	private static int height;
 
-		/*if (args.length != 4) {
-			System.err.println("Usage: java RobotWindowCounterTester <room_width> <room_height> <robot_x_coord> <robot_y_coord>");
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Usage: java PietMondrianTester " + 
+				"<picture_width> <picture_height>");
 			System.exit(-1);
 		}
-		int roomWidth = 3;
-		int roomHeight = 3;
-		int robotInitPosX = 1;
-		int robotInitPosY = 1;
 		try {
-			roomWidth = Integer.parseInt(args[0]);
-			roomHeight = Integer.parseInt(args[1]);
-			robotInitPosX = Integer.parseInt(args[2]);
-			robotInitPosY = Integer.parseInt(args[3]);
+			width = Integer.parseInt(args[0]);
+			height = Integer.parseInt(args[1]);
+			if (width < 1 || height < 1) {
+				throw new NumberFormatException();
+			}
 		} catch (NumberFormatException ex) {
-			System.err.println("Must be integers: <room_width> <room_height> <robot_x_coord> <robot_y_coord>");
+			System.err.println("Must be positive integers: " + 
+				"<picture_width> <picture_height>");
 			System.exit(-1);
 		}
-		Layout[][] roomPlan = Room.generateRoomPlan(roomWidth, roomHeight);
-		// if there's no place for the robot
-		if (roomWidth < 3 || roomHeight < 3) {
-			printRoom(roomPlan);
-			return;
-		}
-		// if the robot's coordinates are out of the floor
-		if (robotInitPosX <= 0 || robotInitPosX >= roomWidth - 1 ||
-			robotInitPosY <= 0 || robotInitPosY >= roomHeight - 1) {
-			printRoom(roomPlan);
-			System.out.println("Error: Robot's position is out of bounds of the floor");
-			return;
-		}
-		// place the robot in the room
-		Robot robot = new Robot(robotInitPosX, robotInitPosY);
-		roomPlan[robotInitPosY][robotInitPosX] = Layout.ROBOT;
-		printRoom(roomPlan);
-		// if the robot is not in a corner or at a border,
-		// move it to the right border
-		if (robotInitPosX != 1 && robotInitPosX != roomWidth - 2 &&
-			robotInitPosY != 1 && robotInitPosY != roomHeight - 2) {
-			while (robot.getX() < roomWidth - 2) {
-				makeMove(robot, roomPlan);
-			}
-		}
-		// face the robot left if it's in the top row
-		// or in the first left column except if it's
-		// in the top right corner or in the bottom
-		// left corner
-		if (robotInitPosY == 1 && robotInitPosX != roomWidth - 2 ||
-			robotInitPosX == 1 && robotInitPosY != roomHeight - 2) {
-			robot.turnLeft();
-			robot.turnLeft();
-		}
-		// face the robot up if it's in the top right corner
-		if (robotInitPosY == 1 && robotInitPosX == roomWidth - 2) {
-			robot.turnLeft();
-		}
-		Layout nextUnit = robot.nextUnit(roomPlan);
-		Layout firstUnit = nextUnit;
-		int windowsCount = 0;
-		boolean isWindowContinue = false;
-		int perimeterUnits = 2 * (roomWidth + roomHeight - 2);
-		// move counterclockwise along the wall
-		// until all the units of the inside
-		// perimeter (perimeterUnits - 4 corner units) are traversed
-		while (perimeterUnits > 4) {
-			nextUnit = robot.nextUnit(roomPlan);
-			if (nextUnit == Layout.FLOOR) {
-				makeMove(robot, roomPlan);
-				robot.turnRight();
-			} else if (nextUnit == Layout.WALL) {
-				isWindowContinue = false;
-				robot.turnLeft();
-				perimeterUnits--;
-			} else if (nextUnit == Layout.WINDOW && !isWindowContinue) {
-				isWindowContinue = true;
-				windowsCount++;
-				robot.turnLeft();
-				perimeterUnits--;
-			} else if (nextUnit == Layout.WINDOW) {
-				isWindowContinue = true;
-				robot.turnLeft();
-				perimeterUnits--;
-			}
-		}
-		printRoom(roomPlan);
-		// we must not count the first window twice if it's long
-		// and we've started and finished traversing at its middle
-		if (firstUnit == Layout.WINDOW && nextUnit == Layout.WINDOW) {
-			windowsCount--;
-		}
-		System.out.println("The number of windows is " + windowsCount);*/
+		Layout[][] canvas = Canvas.generateCanvas(width, height);
+		printCanvas(canvas);
 	}
-
-	/*private static void makeMove(Robot robot, Layout[][] roomPlan) throws InterruptedException {
-		roomPlan[robot.getY()][robot.getX()] = Layout.FLOOR;
-		robot.move();
-		roomPlan[robot.getY()][robot.getX()] = Layout.ROBOT;
-		printRoom(roomPlan);
-		Thread.sleep(80);
-	}*/
 
 	private static void printCanvas(Layout[][] canvas) {
 		System.out.println();
