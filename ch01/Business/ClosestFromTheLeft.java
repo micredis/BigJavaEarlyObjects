8a8bbc4982fcedfa2847a26d1e1a123726f77830
micredis <micredis@gmail.com>
2018-04-01 12:13:35 +0300

    Add initial commit for ClosestFromTheLeft

---
+++ ch01/Business/ClosestFromTheLeft.java
@@ -0,0 +1,17 @@
+/* Suppose you received a loyalty promotion that lets you purchase one item, valued
+up to $100, from an online catalog. You want to make the best of the offer. You have
+a list of all items for sale, some of which are less than $100, some more. Write an
+algorithm to produce the item that is closest to $100. If there is more than one such
+item, list them all. Remember that a computer will inspect one item at a time––it
+can’t just glance at a list and find the best one.
+*/
+
+public class ClosestFromTheLeft {
+	public static void main(String[] args) {
+		new ClosestFromTheLeft().go();
+	}
+
+	public void go() {
+		List<Double> bargains = new ArrayList<>();
+	}
+}
