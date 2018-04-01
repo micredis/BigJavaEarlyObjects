2248404b170e2b556f9190db694a43267270cdc7
micredis <micredis@gmail.com>
2018-04-02 01:09:39 +0300

    Complete the ClosestFromTheLeft problem to operate with defaultCatalog

--- ch01/Business/ClosestFromTheLeft.java
+++ ch01/Business/ClosestFromTheLeft.java
@@ -6,12 +6,56 @@ item, list them all. Remember that a computer will inspect one item at a time–
 can’t just glance at a list and find the best one.
 */
 
+import java.util.*;
+
 public class ClosestFromTheLeft {
+	private static final Map<String, Double> defaultCatalog;
+	static {
+		Map<String, Double> map = new HashMap<>();
+		map.put("Robot Mower", 89.50);
+		map.put("Robot Mower Mark II", 99.90);
+		map.put("Robot Windows Counter", 24.99);
+		map.put("Mazebot", 74.99);
+		map.put("A Maze Bot - 22", 85.55);
+		map.put("Robot Windows Counter Gen 3", 49.99);
+		map.put("Duster Bot", 85.55);
+		map.put("Duster Bot Gen 2", 99.90);
+		map.put("Iron Gardner", 48.80);
+		map.put("Fullerene Gardner", 58.85);
+		defaultCatalog = Collections.unmodifiableMap(map);
+	}
+	
 	public static void main(String[] args) {
 		new ClosestFromTheLeft().go();
 	}
 
 	public void go() {
-		List<Double> bargains = new ArrayList<>();
+		Map<String, Double> bargains = findBargains(ClosestFromTheLeft.defaultCatalog);
+		printMap(bargains);
+	}
+
+	private Map<String, Double> findBargains(Map<String, Double> catalog) {
+		Map<String, Double> bargains = new HashMap<>();
+		double bestPrice = 0;
+		for (Map.Entry<String, Double> item : catalog.entrySet()) {
+			double price = item.getValue();
+			if (price >= bestPrice && price <= 100 &&
+				!bargains.containsValue(price)) {
+				bestPrice = price;
+				bargains.clear();
+				bargains.put(item.getKey(), price);
+			} else if (price >= bestPrice && price <= 100) {
+				bargains.put(item.getKey(), price);
+			}
+		}
+		return bargains;
+	}
+
+	private void printMap(Map<String, Double> map) {
+		System.out.println("Item(s) that is/are closest to $100.00:");
+		System.out.println("----------------------------------------");
+		for (Map.Entry<String, Double> entry : map.entrySet()) {
+			System.out.format("%-30s | $%.2f%n", entry.getKey(), entry.getValue());
+		}
 	}
 }
