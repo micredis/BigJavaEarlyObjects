4410edbddeb0e5d581f9ff6bd7391c0200a37dce
micredis <micredis@gmail.com>
2019-01-24 13:50:51 +0200

    Update DaysAliveAssign to correspond the original assignment (use Day() instead of LocalDate.now()). Add worked_example_2 initial commit with the data provided in the assignment

--- ch02/worked_example_1/DaysAliveAssign.java
+++ ch02/worked_example_1/DaysAliveAssign.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 public class DaysAliveAssign {
 	public static void main(String[] args) {
 		try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-			LocalDate todayDate = LocalDate.now();
-			Day today = new Day(todayDate.getYear(), todayDate.getMonthValue(), todayDate.getDayOfMonth());
+			//LocalDate todayDate = LocalDate.now();
+			//Day today = new Day(todayDate.getYear(), todayDate.getMonthValue(), todayDate.getDayOfMonth());
+			//The above 2 lines represent more universal way of obtaining a current date.
+			//But according to the assignment, we should use instead the provided Day API:
+			Day today = new Day();
 			System.out.println("Enter the day you were born using the format: yyyy-mm-dd");
 			String dobString = console.readLine();
 			String[] dobParsed = dobString.split("-");
